176c43ac7364972816332317dbb2b0edee101012
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-08-30 21:53:52 +0100

    Add all project files

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
     </div>
 
     <!-- <a href="../index.php" class="home_link">Home</a> -->
-    <a href="biology02.php" class="practice btn btn-primary mt-3 px-4 float-end mx-2">Practice</a>
+    <!-- <a href="biology02.php" class="practice btn btn-primary mt-3 px-4 float-end mx-2">Practice</a> -->
 
     <div class="row container mt-4 pt-3">
         <div class="col-sm ">
