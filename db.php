86a42f2c9afa3bb9fc7f528b0bb9b2d5d2a65997
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-08-30 11:10:30 +0100

    Add all project files

--- db.php
+++ db.php
@@ -2,16 +2,16 @@
 session_start();
 
 // Supabase Postgres connection using PDO with environment variables
-// $host = getenv('DB_HOST');
-// $port = getenv('DB_PORT');
-// $dbname = getenv('DB_NAME');
-// $user = getenv('DB_USER');
-// $password = getenv('DB_PASSWORD');
-$host = "aws-0-eu-north-1.pooler.supabase.com";
-$port = 6543;
-$dbname = "postgres";
-$user = "postgres.idgrfypntnjlphmqqgnp";
-$password = "2brhUakI42BbmuwG";
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+// $host = "aws-0-eu-north-1.pooler.supabase.com";
+// $port = 6543;
+// $dbname = "postgres";
+// $user = "postgres.idgrfypntnjlphmqqgnp";
+// $password = "2brhUakI42BbmuwG";
 
 try {
     $conn = new PDO(
